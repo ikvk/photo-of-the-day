fd4dd5252c7e33ca6c3dbe2ecb8fb34557446a11
v.kaukin <v.kaukin@u6.ru>
2022-01-21 11:09:59 +0500

    view_all_in_browser

--- check.py
+++ check.py
@@ -1,26 +1,37 @@
 from wallpaper import *
+import webbrowser
 
-all_get = [
+get_url_fn_set = {
     get_bing_url,
     get_35photo_url,
     get_nasa_url,
     get_astropix_url,
     get_geo_url,
     get_esa_url,
-]
+}
 
 
 def get_all_urls():
-    for get_func in all_get:
-        print(get_func, get_func())
+    result = []
+    for get_func in get_url_fn_set:
+        url = get_func()
+        print(get_func, url)
+        result.append(url)
+    return result
 
 
 def download_all():
-    for get_func in all_get:
+    for get_func in get_url_fn_set:
         print(get_func, download_image_by_url(get_func(), PATH_TO_SAVE_WALLPAPER))
 
 
-get_all_urls()
+def view_all_in_browser():
+    for url in get_all_urls():
+        webbrowser.open(url, new=2)
+
+
+# get_all_urls()
+view_all_in_browser()
 # download_all()
 # print(get_yandex_url())
 # print(get_geo_url())
